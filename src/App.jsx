b869e3d9a45d956e898a475e4dd3813fec32f4ef
vijaydevguy <vijayakumarjob01@gmail.com>
2026-08-12 23:09:32 +0530

    basic_routers

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./pages/Body";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-     
       <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<Body />} />
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
       {/* <h1 className="">Get started</h1> */}
--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const Body = () => {
   return (
     <div>
          <Navbar />
-         body
+         <Outlet/>
     </div>
   )
 }
