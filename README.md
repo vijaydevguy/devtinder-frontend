703ba0053900bab76f407a85d9f1b341d0dd8176
vijaydevguy <vijayakumarjob01@gmail.com>
2026-08-13 12:51:05 +0530

    service

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Config tailwind css
 2. Config daisy ui component package
 3. Setup react router
+4. Login ui with formik
---
+++ src/services/authService.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000";
+
+export const loginService = async (payload) => {
+  const res = await axios.post(`${baseUrl}/signup`, payload);
+  return res;
+};
