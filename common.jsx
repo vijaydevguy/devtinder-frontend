cb89c8c07e09250222b37471309385398eb04d28
vijaydevguy <vijayakumarjob01@gmail.com>
2026-08-29 21:37:46 +0530

    url

--- common.jsx
+++ common.jsx
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:5000";
-// export const baseUrl = "https://devtinder-backend-lgug.onrender.com";
+// export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://devtinder-backend-lgug.onrender.com";
